96b877954b9abee28062465679d90940c47322a6
양태웅 <didxodnd0422@gmail.com>
2022-03-10 17:32:55 +0900

    Add Room library dependency

--- app/src/main/java/com/example/soccerholic/util/Constants.kt
+++ app/src/main/java/com/example/soccerholic/util/Constants.kt
@@ -6,4 +6,7 @@ object Constants {
 
     const val API_HOST = "api-football-v1.p.rapidapi.com"
     const val API_KEY = "13c79588f3msh51b92ec4401b029p15a538jsn6ee2acc72d18"
+
+    const val DATABASE_NAME = "favorite_team_database"
+    const val TABLE_NAME = "favorite_team_table"
 }
