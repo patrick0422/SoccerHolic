6c9532757423290c6ab4787c13ba0ea61120b869
양태웅 <didxodnd0422@gmail.com>
2022-03-12 16:31:36 +0900

    Add MainViewModel

--- app/src/main/java/com/example/soccerholic/ui/MainViewModel.kt
+++ app/src/main/java/com/example/soccerholic/ui/MainViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.soccerholic.ui
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.soccerholic.data.local.TeamBookmarkDao
+import com.example.soccerholic.data.local.TeamBookmarkEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val dao: TeamBookmarkDao
+) : ViewModel() {
+
 }
