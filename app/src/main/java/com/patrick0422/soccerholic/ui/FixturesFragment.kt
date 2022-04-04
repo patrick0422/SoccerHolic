d7744911c055d7ec239f5beae117576097539adf
양태웅 <didxodnd0422@gmail.com>
2022-04-04 11:08:26 +0900

    Add FixturesFragment

---
+++ app/src/main/java/com/patrick0422/soccerholic/ui/FixturesFragment.kt
@@ -0,0 +1,16 @@
+package com.patrick0422.soccerholic.ui
+
+import com.patrick0422.soccerholic.R
+import com.patrick0422.soccerholic.base.BaseFragment
+import com.patrick0422.soccerholic.databinding.FragmentFixturesBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FixturesFragment : BaseFragment<FragmentFixturesBinding>(R.layout.fragment_fixtures) {
+
+
+    override fun init() {
+
+    }
+
+}
